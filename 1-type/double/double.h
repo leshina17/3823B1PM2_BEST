618827370528a5ae805df15aa3f148461c5cef41
leshina17 <alyhsadmai@gmail.com>
2023-10-12 16:31:11 +0300

    new

--- 1-type/double/double.h
+++ 1-type/double/double.h
@@ -1,11 +1,22 @@
+#include <math.h>
 bool compare_double(double x, double y) {
-    return true; // Not implementation
-}
+        if ((-0.000001<=x-y) && (x-y<=0.0000001))
+            return true;
+        else 
+            return false;
+    }
 
 int get_nearest_int(double x) {
-    return 0; // Not implementation
-}
+        if (x+0.5>=(int)x+1)
+            x=(int)x+1;
+        else 
+            x=(int)x;
+    }
 
 double get_fractional(double x) {
-    return 0.1; // Not implementation
+        if (x>=0)
+            return x=x-(int)x;
+        else {
+            return x-((int)x-1);
+        }
 }
