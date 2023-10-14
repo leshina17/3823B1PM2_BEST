fe5c183d6ab09522eb31dd96694d69ef8317cfbf
leshina17 <alyhsadmai@gmail.com>
2023-10-14 21:32:36 +0300

    new

--- 1-type/decomposition/decomposition.h
+++ 1-type/decomposition/decomposition.h
@@ -1,5 +1,5 @@
 int reverse(int number) {
-    int arr[4]={number/1000,number/100,number/10,number%10};
+    int arr[4]={number/1000,(number/100)%10,(number/10)%10,number%10};
     if (arr[3]!=0)
         return arr[3]*1000+arr[2]*100+arr[1]*10+arr[0];
     if (arr[3]==0){
@@ -13,9 +13,17 @@ int reverse(int number) {
 }
 
 double decomposition(int number) {
-    return 0; // Not implementation
+    int x1,x2,x3,x4;
+    x1=(number/1000)*1000;
+    x2=((number/100)%10)*100;
+    x3=((number/10)%10)*10;
+    x4=number%10;
+    return (double)(4/((1.0/x1)+(1.0/x2)+(1.0/x3)+(1.0/x4)));
 }
 
 int append(int number, int start, int end) {
-    return 0; // Not implementation
+    if (start>0)
+        return (start*10000+number)*10+end;
+    else 
+        return (start*10000-number)*10-end;
 }
