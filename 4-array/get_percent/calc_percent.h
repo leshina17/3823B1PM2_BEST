38da059cad974c8efdd0c2595dcd5ad392cc72d4
leshina17 <alyhsadmai@gmail.com>
2023-11-01 22:42:54 +0300

    d

--- 4-array/get_percent/calc_percent.h
+++ 4-array/get_percent/calc_percent.h
@@ -1,5 +1,22 @@
 #include <math.h>
 
-int calc_percent(int matrix[], int size){
-    return 0;
+int calc_percent(int a[], int size){
+    int max,min,r,c;
+    min=10000000;
+    max=0;
+    c=0;
+    for (int i=0;i<size;i++){
+        if (a[i]>max)
+            max=a[i];
+    }
+    for (int i=0;i<size;i++){
+        if (a[i]<min)
+            min=a[i];
+    }
+    r=(max-min)/2;
+    for (int i=0;i<size;i++){
+        if (a[i]>=r)
+            c+=1;
+    }
+    return (double)(c/size)*100;
 }
