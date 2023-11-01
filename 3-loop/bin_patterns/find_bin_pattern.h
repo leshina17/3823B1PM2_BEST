81ecbb1517b34253f2954fe6675bf0219bc898b8
leshina17 <alyhsadmai@gmail.com>
2023-11-02 01:15:03 +0300

    o

--- 3-loop/bin_patterns/find_bin_pattern.h
+++ 3-loop/bin_patterns/find_bin_pattern.h
@@ -1,5 +1,19 @@
 #include <math.h>
 
-char find_bin_pattern(int number){
-    return 100;
+char find_bin_pattern(int n){
+    int c,s;
+    s=0;
+    c=0;
+    int a[10000];
+    while (n>=2){
+        a[s]=n%2;
+        n=n/2;
+        s+=1;
+    }
+    a[s]=n;
+    for (int i=1;i<s;i++){
+        if ((a[i]==0)&&(a[i+1]==1)&&(a[i-1]==1))
+            c+=1;
+    }
+    return c;
 }
