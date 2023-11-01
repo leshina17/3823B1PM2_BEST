8fe38639c97e76a98bdad94e049017bd2b0ca4f2
leshina17 <alyhsadmai@gmail.com>
2023-11-01 23:04:46 +0300

    g

--- 3-loop/row/calc_row.h
+++ 3-loop/row/calc_row.h
@@ -1,3 +1,11 @@
 long long calc_row(int n, long long k){
-    return 0;
+    long long r;
+    r=0;
+    for (int i=1;i<n+1;i++){
+        r=i*k;
+    }
+    if (r> 9223372036854775807)
+        return 0;
+    else 
+        return r;
 }
