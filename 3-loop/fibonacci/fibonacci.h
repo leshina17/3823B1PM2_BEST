975149de2b733a48de2aee3c49cd3b995136d8de
leshina17 <alyhsadmai@gmail.com>
2023-11-03 00:21:43 +0300

    l

--- 3-loop/fibonacci/fibonacci.h
+++ 3-loop/fibonacci/fibonacci.h
@@ -1,10 +1,16 @@
 long fib(int m){
     int s;
     s=2;
-    int a[1000]={1,1};
-    for (int i=2;i<=m;i++){
-        a[i]=a[i-2]+a[i-1];
-        s+=a[i];
+    if (m>0){
+        int a[1000]={1,1};
+        for (int i=2;i<1000;i++){
+            a[i]=a[i-2]+a[i-1];
+            if (a[i]>m)
+                return s;
+            s+=a[i];
+        }
+        return s;
     }
-    return s;
+    else 
+        return -1;
 }
