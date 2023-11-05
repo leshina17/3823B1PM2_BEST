7cd0c6d70514408bf63aef5f5293e4dcaa0f036f
leshina17 <alyhsadmai@gmail.com>
2023-11-05 16:55:54 +0300

    g

--- 3-loop/find_digit/find_digit.h
+++ 3-loop/find_digit/find_digit.h
@@ -1,5 +1,37 @@
 #include <math.h>
 
-char find_digit(double number, char digit){
-    char c;
+char find_digit(double nn, char d){
+    int s, x, r;
+    s=0;
+    x=0;
+    r=0;
+    long long n;
+    n=nn;
+    if ((d<0)||(d>9))
+        return -1;
+    for (int i=0;n>0;i++){
+        s++;
+        n/=10;
+    }
+    for (int i=1;i<s+1;i++){
+        if (nn>=1){
+            x=(int)(nn/(pow(10,s-i)))%10;
+            if (x==d){
+                r=i;
+                break;
+            }
+        }
+    }
+    if (r==0){
+        nn=nn-(int)nn;
+        for (int i=s+1;nn>0;i++){
+            x=(int)(nn*10);
+            nn=nn*10-x;
+            if (x==d){
+                r=i+1;
+                break;
+            }
+        }
+    }
+    return r;
 }
--- 3-loop/limit/limit.h
+++ 3-loop/limit/limit.h
@@ -1,5 +1,15 @@
 #include <math.h>
 
 double limit(double eps, int n){
-    return n*n+n;
+    double x,xx,r,g;
+    r=0;
+    g=0;
+    for (int i=1;i<n+1;i++){
+        x=(i)/(sqrt(i*i+i));
+        xx=(i+1)/(sqrt((i+1)*(i+1)+i+1));
+        r=fabs(xx-x);
+        if (r<eps)
+            return xx;
+    }
+    return x;
 }
