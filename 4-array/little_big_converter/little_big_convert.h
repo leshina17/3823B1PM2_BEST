7a2b2ce8c9ab935d480863e4c790d0be4278db93
leshina17 <alyhsadmai@gmail.com>
2023-11-13 22:23:38 +0300

    p

--- 4-array/little_big_converter/little_big_convert.h
+++ 4-array/little_big_converter/little_big_convert.h
@@ -1,5 +1,9 @@
 #include <math.h>
 
 unsigned int little_big_convert(unsigned int input){
-    return 0;
+    unsigned char b1 = (unsigned char)(input >> 24);
+    unsigned char b2 = (unsigned char)(input >> 16);
+    unsigned char b3 = (unsigned char)(input >> 8);
+    unsigned char b4 = (unsigned char)input;
+    return b4 *256*256*256 + b3 * 256 * 256 + b2 * 256 + b1;
 }
