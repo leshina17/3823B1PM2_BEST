0dce8455498c0c9adcd9e595cf22bae7b8cd5b7c
leshina17 <alyhsadmai@gmail.com>
2023-12-19 20:38:57 +0300

    k

--- 8-strings/calc_expresson/calc_expression_value.h
+++ 8-strings/calc_expresson/calc_expression_value.h
@@ -2,25 +2,5 @@
 #include <string.h>
 
 int calculate_expression(char exp[]){
-    int x=0;
-    int arr[10];
-    for (int i=0; i<strlen(exp); i+2){
-        for (int j=0; j<10; j++){
-            arr[j]=exp[i];
-            i+2;
-            }
-        }
-    x=arr[0];
-    for (int i=1; i<strlen(exp); i+2){
-        for (int j=0; j<10; j++){
-            if (exp[i]=='+')
-                x=x+arr[j+1];
-            if (exp[i]=='-')
-                x=x-arr[j+1];
-            else 
-                return 0;
-        }
-    }
-    return x;
-    //return 0;
+    
 }
